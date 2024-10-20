32c06313a7eb43a8eed0241ce8829ddca5f89ec8
robinboywonder <49100318+robinboywonder@users.noreply.github.com>
2024-10-20 14:20:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
     try {
         console.log('Started refreshing application (/) commands.');
         await rest.put(
-            Routes.applicationCommands('YOUR_APPLICATION_ID'), 
+            Routes.applicationCommands('1297628508021067846'), 
             { body: commands },
         );
         console.log('Successfully reloaded application (/) commands.');
